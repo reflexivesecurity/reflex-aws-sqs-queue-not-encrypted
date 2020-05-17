33e3b5fc9acba0c6004af1d2385cfa16724f767f
rjulian <richard@rjulian.net>
2020-05-17 13:37:31 -0700

    Multiregion refactor

---
+++ terraform/cwe/main.tf
@@ -0,0 +1,26 @@
+module "cwe" {
+  source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe?ref=v0.6.0"
+  name        = "SqsQueueNotEncrypted"
+  description = "Rule to enforce SQS queue encryption"
+
+  event_pattern = <<PATTERN
+{
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "source": [
+    "aws.sqs"
+  ],
+  "detail": {
+    "eventSource": [
+      "sqs.amazonaws.com"
+    ],
+    "eventName": [
+      "CreateQueue",
+      "SetQueueAttributes"
+    ]
+  }
+}
+PATTERN
+
+}
---
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -0,0 +1,35 @@
+module "sqs_lambda" {
+  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v0.6.0"
+
+  cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
+  cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
+  function_name             = "SqsQueueNotEncrypted"
+  source_code_dir           = "${path.module}/../../source"
+  handler                   = "sqs_queue_not_encrypted.lambda_handler"
+  lambda_runtime            = "python3.7"
+  environment_variable_map  = { SNS_TOPIC = var.sns_topic_arn }
+  custom_lambda_policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "sqs:GetQueueAttributes",
+        "sqs:SetQueueAttributes"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+
+  queue_name    = "SqsQueueNotEncrypted"
+  delay_seconds = 0
+
+  target_id = "SqsQueueNotEncrypted"
+
+  sns_topic_arn  = var.sns_topic_arn
+  sqs_kms_key_id = var.reflex_kms_key_id
+}
