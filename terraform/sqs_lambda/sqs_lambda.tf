8e4730df35016828ced509e164ee01b47bc74eeb
Max Anderson <maxwell.c.anderson@gmail.com>
2020-08-12 14:10:01 -0700

    Update to reflex-engine v2.0.0

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,17 +1,17 @@
 module "sqs_lambda" {
-  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v1.0.0"
+  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v2.0.0"
 
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
   function_name             = "SqsQueueNotEncrypted"
-  source_code_dir           = "${path.module}/../../source"
+  package_location          = var.package_location
   handler                   = "sqs_queue_not_encrypted.lambda_handler"
   lambda_runtime            = "python3.7"
-  environment_variable_map  = {
+  environment_variable_map = {
     SNS_TOPIC = var.sns_topic_arn,
     MODE      = var.mode
   }
-  custom_lambda_policy      = <<EOF
+  custom_lambda_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
