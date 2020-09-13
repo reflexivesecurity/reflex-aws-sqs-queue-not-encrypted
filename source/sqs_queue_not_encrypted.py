7fd6ccb236da89114b9316cde6ce0bac191c1f45
rjulian <richard@rjulian.net>
2020-09-13 14:46:08 -0700

    Semi-automated multi account push

--- source/sqs_queue_not_encrypted.py
+++ source/sqs_queue_not_encrypted.py
@@ -4,7 +4,7 @@ import json
 
 import boto3
 
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class SqsQueueNotEncrypted(AWSRule):
@@ -55,5 +55,8 @@ class SqsQueueNotEncrypted(AWSRule):
 def lambda_handler(event, _):
     """ Handles the incoming event """
     print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = SqsQueueNotEncrypted(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
